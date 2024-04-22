8aa5393239e2bac009102b71cb846e660c2bf43d
aysenazgelen <aysenazgelen@gmail.com>
2024-04-22 14:56:57 +0300

    hmm

--- Se-116-Project/heheh.java
+++ Se-116-Project/heheh.java
@@ -1,5 +1,15 @@
+import java.util.Scanner;
+
 public class heheh {
     public static void main(String[] args) {
         System.out.println("muhehehe");
+        Scanner scanner = new Scanner(System.in);
+        System.out.print("Enter an integer: ");
+        int num = scanner.nextInt();
+
+        for (int i = 0; i < num; i++) {
+            System.out.println("hebele hübele");
+        }
+        scanner.close();
     }
 }
