72d3229374666bee7da58dd4d02a0f0de74ea707
fangfurtail <kaan.bilgili@std.izmirekonomi.edu.tr>
2024-05-18 18:26:28 +0300

    more practical checker for Task.java
    Found a better way for checking and saving task type of each task.

--- Se-116-Project/calisanlar/Task.java
+++ Se-116-Project/calisanlar/Task.java
@@ -1,3 +1,6 @@
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
 class Task{
     private String name;
     private float size;
@@ -17,102 +20,18 @@ class Task{
         this.taskType = taskType;
     }
     public void setTaskType(String t) {
-        if(t.equals("T0")){
-            this.taskType = taskType.T0;
+        // Tries setting the enum value, if the value doesn't exist in the enum, it will throw an error and terminate the program.
+        try{
+            this.taskType = taskType.valueOf(t);
         }
-        else if(t.equals("T1")){
-            this.taskType = taskType.T1;
-        }
-        else if(t.equals("T2")){
-            this.taskType = taskType.T2;
-        }
-        else if(t.equals("T3")){
-            this.taskType = taskType.T3;
-        }
-        else if(t.equals("T4")){
-            this.taskType = taskType.T4;
-        }
-        else if(t.equals("T5")){
-            this.taskType = taskType.T5;
-        }
-        else if(t.equals("T6")){
-            this.taskType = taskType.T6;
-        }
-        else if(t.equals("T7")){
-            this.taskType = taskType.T7;
-        }
-        else if(t.equals("T8")){
-            this.taskType = taskType.T8;
-        }
-        else if(t.equals("T9")){
-            this.taskType = taskType.T9;
-        }
-        else if(t.equals("T10")){
-            this.taskType = taskType.T10;
-        }
-        else if(t.equals("T11")){
-            this.taskType = taskType.T11;
-        }
-        else if(t.equals("T12")){
-            this.taskType = taskType.T12;
-        }
-        else if(t.equals("T13")){
-            this.taskType = taskType.T13;
-        }
-        else if(t.equals("T14")){
-            this.taskType = taskType.T14;
-        }
-        else if(t.equals("T15")){
-            this.taskType = taskType.T15;
-        }
-        else if(t.equals("T16")){
-            this.taskType = taskType.T16;
-        }
-        else if(t.equals("T17")){
-            this.taskType = taskType.T17;
-        }
-        else if(t.equals("T18")){
-            this.taskType = taskType.T18;
-        }
-        else if(t.equals("T19")){
-            this.taskType = taskType.T19;
-        }
-        else if(t.equals("T20")){
-            this.taskType = taskType.T20;
-        }
-        else if(t.equals("T21")){
-            this.taskType = taskType.T21;
-        }
-        else if(t.equals("T22")){
-            this.taskType = taskType.T22;
-        }
-        else if(t.equals("T23")){
-            this.taskType = taskType.T23;
-        }
-        else if(t.equals("T24")){
-            this.taskType = taskType.T24;
-        }
-        else if(t.equals("T25")){
-            this.taskType = taskType.T25;
-        }
-        else if(t.equals("T26")){
-            this.taskType = taskType.T26;
-        }
-        else if(t.equals("T27")){
-            this.taskType = taskType.T27;
-        }
-        else if(t.equals("T28")){
-            this.taskType = taskType.T28;
-        }
-        else if(t.equals("T29")){
-            this.taskType = taskType.T29;
-        }
-        else if(t.equals("T30")){
-            this.taskType = taskType.T30;
-        }
-        else{
-            System.err.println("Wrong tasktype.\nTerminating...");
-            System.out.println(t);
+        catch(IllegalArgumentException e){
+            Pattern p = Pattern.compile("TaskType\\.([A-z0-9_. ]+)");
+            Matcher m = p.matcher(e.getMessage());
+            String invalidTaskID = "";
+            if(m.find()){
+                invalidTaskID = m.group(1);
+            }
+            System.err.println("Not a valid TaskType: " + invalidTaskID + "\nTerminating...");
             System.exit(1);
         }
     }
